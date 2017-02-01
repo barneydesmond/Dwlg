44730089fe5612ef2fdb9a8d76d6e8ac7c4816cb
daljal <medalijallouli@gmail.com>
2017-02-01 15:30:17 +0100

    Add files via upload

--- Dwlg.c
+++ Dwlg.c
@@ -2,27 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    int a;
+    int b;
+
+}dret;
+
 void logo(){
 
-printf("\n         ;1111111111111111111111111;,\n");
-printf("         f00000000000000G           .G000G.\n");
-printf("     ,ttti;;;....0000....              ,00000\n");
-printf("     ,fffi:::    0000....                000000\n");
-printf("         f000::::    0008                 G00000.\n");
-printf("         f000::::    0008                  000000\n ");
-printf("     ,fff:   ffff00000008                  t000000\n");
-printf("     ,fff:   ffff00000008                  .000000L\n");
-printf("                 0000....                   0000000\n ");
-printf("                 0000...                   0000000\n");
-printf("             G000tfft0008                   0000000\n");
-printf("             G000tfft0008                   0000000\n");
-printf("         :000,   ::::0008                   000000G\n");
-printf("         :000,   ::::0008                  :000000\n");
-printf("                     0008                  800000,\n");
-printf("                     0008                 f00000.\n");
-printf("                 00000008                L0000G\n");
-printf("                 00000000              ,0000t\n");
-printf("                 0000000000L,.   ..;0000L\n\n");
+printf("\n       CCCf.                                           ;LCCC       ");
+printf("\n       tCCCCCCCL                                  ;CCCCCCCC        ");
+printf("\n        CCt  ;CCCCCt                           CCCCC;  ;CC1        ");
+printf("\n         CC      :CCCC,                     LCCC:      CCC         ");
+printf("\n         .CC        ,CCCt                 CCC,        LCC          ");
+printf("\n          .CC          CCC.             CCC          fCC           ");
+printf("\n            CC          ,CCC           CC.          LCf            ");
+printf("\n             tCL          CCCCCCCCCCCCCC           CC              ");
+printf("\n               CCi        .CCCCCCCCCCCC.         CC;               ");
+printf("\n                 LCC       CCCCCCCCCCCC        CC,                 ");
+printf("\n                    CCC   CCCCCCCCCCCCCC    CCf                    ");
+printf("\n                       .,CCCCCCCCCCCCCCCCt .                       ");
+printf("\n                      ;CCC:   iCCCCC    CCCL                       ");
+printf("\n                    CCCCCCC:   LCCC . .CCCCCCC.                    ");
+printf("\n                       fCCCCC   CCC  CCCCCf                        ");
+printf("\n                           ::   CC;  ::                            ");
+printf("\n                                CC                                 ");
+printf("\n                                                                 \n");
+
 
 
 }
@@ -80,7 +87,7 @@ if((c=='Y')||(c=='y')){
            do{
             printf(" (Input_folder) /../filename.txt :   ");
             scanf("%s",sh);
-           } while((sh[0]!='/')||(strlen(sh)<6));
+           } while(!fopen(sh,"r+"));
            ftxt = fopen(sh,"r+");
            }while(!ftxt);
            i=vider(ftxt,m);
@@ -107,9 +114,12 @@ do{
 
 
 
-void maker(FILE* f , char txt[100][100],int t[100],int k,int lmin ,int lmax){
+dret maker(FILE* f , char txt[100][100],int t[100],int k,int lmin ,int lmax,int n){
 int j,i;
 char ch1[100],ch[100];
+dret r;
+
+r.b=1;
 
 ch[0]='\0';
 
@@ -125,17 +135,24 @@ for(i=0;i<k+1;i++){
 
 if((strlen(ch)>=lmin)&&(strlen(ch)<=lmax)){
 fprintf(f,"%s\n",ch);
+printf("%s\n",ch);
+r.a=1;
+}else if(strlen(ch)>lmax){ r.a=-1 ;
+}else{ r.a=1; }
+
+if(t[0]==(n-1)){ r.b=0;}
+
+return r;
+
 
 }
 
-}
 
 
 
 
 
-
-int full(int t[100],int k,int n ,double x ,double fac){
+int full(int t[100],int k,int n ,double x ,double fac,int h){
 int i=0,j=0,b=0,v=0;
 
 if((k==n)&&(t[0]==(n-1))&&(t[n-1]==0)&&(x>=fac)){
@@ -149,13 +166,15 @@ i=-1;
 do{ i++; b=0;
 
     for(j=0;j<k;j++){ if(i==t[j]){b=1;} }
+
        if(t[k]>=0){ if((b==0)&&(i<=t[k])){b=1;v++;} }
 
 }while((b==1));
 
-if (v<n-k){return i;} else if(v>=n-k) {return (-1);}
+if (v<n-k){return i;} else if(v>=n-k+h) {return (-1);}
 
 }
+
 }
 
 double factor(int n){
@@ -166,23 +185,28 @@ double factor(int n){
 
         }
 
+
+
+
 void caller(char txt[100][100],int t[100], int n , FILE* f,int lmin,int lmax){
 
-int i=0;
+int i=0,e=0;
 double fac,x=1;
+dret s;
 
 for(i=0;i<n;i++){
     t[i]=i;
-    maker(f,txt,t,i,lmin,lmax);
+    maker(f,txt,t,i,lmin,lmax,n);
 }
 
 fac=factor(n);
 
 do{
 
-if(i==n){ if(((100*x)*1.2/fac)<=100){  system("clear"); printf("NOW:[%lf%]\n",((100*x)*1.2/fac)); } else{ system("clear"); printf("NOW:[100%]\n"); }  }
 
-        t[i]=full(t,i,n,x,fac);   
+       if((t[0]!=n-1)||(i!=0)||(n==999)){
+        t[i]=full(t,i,n,x,fac,0);
+       }else{  t[i]=full(t,i,n,x,fac,1);   }
 
     if(t[i]==-1){
      i=i-1;
@@ -190,7 +214,13 @@ if(i==n){ if(((100*x)*1.2/fac)<=100){  system("clear"); printf("NOW:[%lf%]\n",((
         x++;
         t[i-1]=-1;
         i=i-2;
-       }else if(t[i]>=0) { maker(f,txt,t,i,lmin,lmax); i++; }
+       }else if(t[i]>=0) { s=maker(f,txt,t,i,lmin,lmax,n);
+                           i=i+s.a;
+                            if(s.b==0){
+                                     e=1;
+                                     }
+                          }
+if((e==1)&&(t[0]!=n-1)){t[i]=-11;}
 
 }while(t[i]!=-11);
 
@@ -219,23 +249,18 @@ return (strlen(ch));
 }
 
 
+
+
 int main()
 {
-int n,l,lmin=0,lmax=100;
+int n,l,lmin=0,lmax=999;
 char txt[100][100],c,sh[100];
 int t[100];
 FILE* f= NULL;
 logo();
 
-  
-
-
-
 
 n=insert(txt);
-
-
-
 
 
 
