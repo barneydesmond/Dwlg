4091524a6581dde14bef1a108f931f6cb5b9b5ab
daljal <medalijallouli@gmail.com>
2017-02-05 12:53:18 +0100

    Add files via upload

--- lg.h
+++ lg.h
@@ -1,6 +1,13 @@
 #include <time.h>
 
-
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
 
 void logo(){
 int a;
@@ -9,7 +16,7 @@ srand(time(NULL));
 
     switch(a){
 case(1):{
-printf("\n       CCCf.                                           ;LCCC       ");
+printf(CYN "\n       CCCf.                                           ;LCCC       ");
 printf("\n       tCCCCCCCL                                  ;CCCCCCCC        ");
 printf("\n        CCt  ;CCCCCt                           CCCCC;  ;CC1        ");
 printf("\n         CC      :CCCC,                     LCCC:      CCC         ");
@@ -25,13 +32,13 @@ printf("\n                      ;CCC:   iCCCCC    CCCL                       ");
 printf("\n                    CCCCCCC:   LCCC . .CCCCCCC.                    ");
 printf("\n                       fCCCCC   CCC  CCCCCf                        ");
 printf("\n                           ::   CC;  ::                            ");
-printf("\n                                CC                                 ");
+printf("\n                                CC                                 "RESET);
 printf("\n\n");
 
 }break;
 
 case(2):{
-printf("\n                                                        ");
+printf(RED"\n                                                        ");
 printf("\n                                                        ");
 printf("\n                                          1             ");
 printf("\n                                         ,L    L.       ");
@@ -55,14 +62,14 @@ printf("\n    :LLLLLL        fLLLLLL:      LLLLLLLLLLLLt          ");
 printf("\n     LLLLL.          :LLLLLf     LLLLLLLLLLLLL          ");
 printf("\n     fLLLL;            .LLLLL.   iLLLLLLLLLLLi          ");
 printf("\n      ,LLLLf              :LLLL:  ,LLLLLLLLL:           ");
-printf("\n         tLLLL1               :LLLL: :LLLL:         \n\n");
+printf("\n         tLLLL1               :LLLL: :LLLL:         \n\n"RESET);
 }
 break;
 
 case (3):
     {
 
-printf("\n                       ,1tLLLft;.                       ");
+printf(RED "\n                       ,1tLLLft;.                       ");
 printf("\n                 tCCCCCCCCCCCCCCCCCCCC,                 ");
 printf("\n             :CCCCCCCCtffffti.   .tCCCCCCC              ");
 printf("\n           CCCCCCCCCCCCCCCCCCCCCCCC   .CCCCCi           ");
@@ -73,38 +80,38 @@ printf("\n     CCC1 fCCCCCCC:              tCCCCCCCC.    CCCL     ");
 printf("\n    CCC;  fCCCCCCC:               ,CCCCCCCC     CCCf    ");
 printf("\n   CCCL   fCCCCCCC:                CCCCCCCCL     CCC.   ");
 printf("\n  .CCC    fCCCCCCC:                :CCCCCCCC     1CCC   ");
-printf("\n  tCCL    fCCCCCCC:         :tLCCCCCCti,. .;    ,;1tf   ");
-printf("\n  CCC;    fCCCCCCC: ;CCCCCCCt     :.00000000            ");
-printf("\n  CCC:    fCCCCCCCCCCCC.    :8f    ,00000000         :  ");
-printf("\n  LCC1    fCCCCCCCt     108.       800000008            ");
-printf("\n  ;CCC    fCCCC      808           00000000i        8   ");
-printf("\n   CCC:   fL     .800.            800000008        0f   ");
-printf("\n   ,CCC        800C              000000000        C8    ");
-printf("\n    tC,     t00000:           G0000000000        80     ");
+printf("\n  tCCL    fCCCCCCC:         :tLCCCCCCti,. .;    ,;1tf   " RESET);
+printf(RED"\n  CCC;    fCCCCCCC: ;CCCCCCCt " BLU "    :.00000000            ");
+printf(RED"\n  CCC:    fCCCCCCCCCCCC.  "BLU"  :8f    ,00000000         :  ");
+printf(RED"\n  LCC1    fCCCCCCCt    "BLU" 108.       800000008            ");
+printf(RED"\n  ;CCC    fCCCC     "BLU" 808           00000000i        8   ");
+printf(RED"\n   CCC:   fL    "BLU" .800.            800000008        0f   ");
+printf(RED"\n   ,CCC       "BLU" 800C              000000000        C8    ");
+printf(RED"\n    tC,    "BLU" t00000:           G0000000000        80     ");
 printf("\n          80000000000000000000000000000f        00      ");
 printf("\n       .00000000000000000000000000000f        000       ");
 printf("\n        00000000000000000000000000t         000,        ");
 printf("\n          00000:                         i000i          ");
 printf("\n            t000000.                 ;80008             ");
 printf("\n               i800000008800088800000000                ");
-printf("\n                    t8000000000008G:                    ");
+printf("\n                    t8000000000008G:                    "RESET);
 printf("\n\n");
     }
 break;
 
 case 4:{
 printf("\n");
-printf("\n                                 ;G.                  CL    ");
-printf("\n                                 ;CCG               LGCL      ");
-printf("\n                                 ;CCCCf           iGCCCL   ");
-printf("\n                                 ;CCCCCC;       ,GCCCCCL      ");
-printf("\n                                 ;CCCCCCCCCCCCCCCCCCCCCL     ");
-printf("\n                                 ;CCCCCCCCCCCCCCCCCCCCCL   ");
-printf("\n                                 ;CCCCCCCCCCCCCCCGGCCCCL      ");
-printf("\n                                 ;CCCt,C,LCCCCG1,C,LCCCL    ");
-printf("\n                                 ;CCCGGGGGCCCCCGGCCCCCCL        ");
-printf("\n                                 ;CCCCCCCCG...GCCCCCCCCL               ");
-printf("\n                          1GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGL       ");
+printf(GRN"\n                                 ;G.                  CL    ");
+printf(YEL"\n                                 ;CCG               LGCL      ");
+printf(BLU"\n                                 ;CCCCf           iGCCCL   ");
+printf(MAG"\n                                 ;CCCCCC;       ,GCCCCCL      ");
+printf(CYN"\n                                 ;CCCCCCCCCCCCCCCCCCCCCL     ");
+printf(WHT"\n                                 ;CCCCCCCCCCCCCCCCCCCCCL   ");
+printf(RED"\n                                 ;CCCCCCCCCCCCCCCGGCCCCL      ");
+printf(GRN"\n                                 ;CCCt,C,LCCCCG1,C,LCCCL    ");
+printf(GRN"\n                                 ;CCCGGGGGCCCCCGGCCCCCCL        ");
+printf(BLU"\n                                 ;CCCCCCCCG...GCCCCCCCCL               ");
+printf(MAG"\n                          1GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGL       "RESET);
 printf("\n\n");
 }break;
 
